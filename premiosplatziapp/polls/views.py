2bd8a863e93134b69cf2334d8dcf244594ad9af9
Paulo Tintaya <pau.tintaya.code@gamil.com>
2022-11-22 09:12:28 -0400

    views added index, detail, results, vote

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(content='Hello World')
+    return HttpResponse(content='Estas en la pagina prinicpal de Premios Platzi App')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero{question_id}")
