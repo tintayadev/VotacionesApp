4e6bd86c863402fe9a969cfc4871acbb3755263e
Paulo Tintaya <pau.tintaya.code@gamil.com>
2022-11-22 09:58:59 -0400

    form views added

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+
+from .models import Question
+
+
 def index(request):
-    return HttpResponse(content='Estas en la pagina prinicpal de Premios Platzi App')
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+        })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
