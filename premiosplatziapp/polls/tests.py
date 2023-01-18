9ff42b5fd2f311a3a9d2fc574116f5651e46eb46
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-01-17 22:00:21 -0400

    tests added for question published in the past, future, and present according to the published date

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Quien es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently() must return Faase for questions whose pub_date is more than 1 day in the past"""
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text="¿Quien es el mejor Course Direct de Platzi?",pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        """was_published_recently() must return True for questions whose pub_date is actual"""
+        time = timezone.now()
+        present_question = Question(question_text="¿Quien es el mejor Course Direct de Platzi?",pub_date=time)
+        self.assertIs(present_question.was_published_recently(), True)
+
+    
