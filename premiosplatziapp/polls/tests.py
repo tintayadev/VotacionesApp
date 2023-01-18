d4830a96c11f33608cb6f48a0cd4b8701b8e7a56
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-01-18 18:50:52 -0400

    templates/index.html typo fixed and also added new tests for the views.py

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,27 +1,71 @@
 import datetime
+from django.http import response
 
 from django.test import TestCase
+from django.urls.base import reverse
 from django.utils import timezone
 
 from .models import Question
-class QuestionModelTests(TestCase):
+class QuestionModelTests(TestCase): 
 
-    def test_was_published_recently_with_future_questions(self):
-        """was_published_recently returns False for questions whose pub_date is in the future"""
+    def test_was_published_recently_with_future_questions(self): 
+        """was_published_recently() returns False for questions whose pub_date is in the future."""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="Quien es el mejor Course Director de Platzi?", pub_date=time)
+        future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_past_questions(self):
-        """was_published_recently() must return Faase for questions whose pub_date is more than 1 day in the past"""
-        time = timezone.now() - datetime.timedelta(days=30)
-        past_question = Question(question_text="¿Quien es el mejor Course Direct de Platzi?",pub_date=time)
-        self.assertIs(past_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        """was_published_recently() returns False for questions whose pub_date is older than one day."""
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_present_questions(self):
+    def test_was_published_recently_with_recent_question(self):
+        """was_published_recently() returns True for questions whose pub_date is within the last day."""
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
         """was_published_recently() must return True for questions whose pub_date is actual"""
         time = timezone.now()
         present_question = Question(question_text="¿Quien es el mejor Course Direct de Platzi?",pub_date=time)
         self.assertIs(present_question.was_published_recently(), True)
 
-    
+
+
+def create_question(question_text, days): 
+    """
+    Create a question with the given 'question_text' and published the given number
+    of 'days' offset to now (negative for questions published in the past, positive
+    for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self): 
+        """If no question exist, an appropiate message is displayed."""
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """Questions with a pub_date in the past are displayed on the index page."""
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on the
+        index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,10 @@
+from datetime import time
+from django.http.response import Http404
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -52,7 +54,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, "polls/detail.html", {
             "question": question,
-            "error_message": "No elegiste una respuesta"
+            "error_message": "You didn't select a choice"
         })
     else:
         selected_choice.votes += 1
