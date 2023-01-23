cda2bb8c7226ffa1f4d7ece99160f216f07b0b2c
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-01-22 22:44:35 -0400

    test created for que question detail view

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     #ex: /polls/
-    path("", views.IndexView.as_view(), name="index"),
-    #ex: /polls/5/
-    path("<int:pk>/detail/", views.DetailView.as_view(), name="detail"),
-    #ex: /polls/5/results/
-    path("<int:pk/results/", views.ResultView.as_view(), name="results"),
-    #ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('', views.IndexView.as_view(), name='index'),
+    #ex: /polls/5
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    #ex: /polls/5/results
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    #ex: /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -9,54 +9,58 @@ from django.views import generic
 from .models import Question, Choice
 
 
-# def index(request):
-#     latest_question_list = Question.objects.all()
-#     return render(request, "polls/index.html", {
-#         "latest_question_list": latest_question_list
-#         })
+# def index(request): 
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     return render(request, "polls/index.html", {"latest_question_list": latest_question_list}) 
 
 
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/detail.html", {
-#         "question": question
-#     })
+#     return render(request, "polls/detail.html", {"question": question})
+
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/results.html",{
-#         "question": question
-#     })
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-    
-    def get_queryset(self):
-        """Return the last five published questions"""
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self): 
+        """Return the last five published questions (not including those set to be published in the future)."""
+        # return Question.objects.order_by('-pub_date')[:5]
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
-    temeplate_name = "polls/detail.html"
+    template_name = 'polls/detail.html'
 
-class ResultView(generic.DetailView):
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultView(generic.DetailView): 
     model = Question
-    template_name = "polls/results.html"
+    template_name = 'polls/results.html'
 
 
-def vote(request, question_id):
+def vote(request, question_id): 
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
+    except (KeyError, Choice.DoesNotExist): 
         return render(request, "polls/detail.html", {
-            "question": question,
-            "error_message": "You didn't select a choice"
+            'question': question,
+            'error_message': "You didn't select a choice"
         })
-    else:
+    else: 
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
