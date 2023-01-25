3b193d3a5fbc1cab26814364dcefe2341be67eaf
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-01-24 22:04:22 -0400

    Modify the admin page to add choice for Questions

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
